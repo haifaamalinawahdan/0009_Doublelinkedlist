4ad6d04b04ac54f050d3024b7454e34e5f7d2f98
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-30 15:54:27 +0700

    menambah nilai pada data fields

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -29,6 +29,9 @@ public :
         // step 1 : Allocate memory for new node
         Node *newNode = new Node();
 
+        // step 2 : Assign value to the data fields
+        newNode->noMhs = nim;
+
     }
 
 
