2bb205ee85cb28649bb35ba9cc9827d4ba79293d
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-30 16:10:58 +0700

    menambahkan while if condition untuk insert in between node

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -52,5 +52,20 @@ public :
             START = newNode;
             return;
         }
+
+        //insert in between  node
+        //step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
     }
 }
