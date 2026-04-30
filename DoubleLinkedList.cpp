bd1b884ef638e822bde4fc9d16e8a8facd6f776f
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-30 16:42:33 +0700

    membuat fungsi untuk mencari data

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -172,7 +172,33 @@ public :
         }
     }
 
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
 
-        
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
 
-}
+        Node *current = START;
+
+        // step 1 : Traverse to find matching roll number
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        // step 2 : Output result
+        if (current == NULL)
+        {
+            cout << "Record not found\n";
+        }
+        else
+        {
+            cout << "Record found\n";
+            cout << "Roll Number: " << current->noMhs << endl;
+        }
+    }
+};
